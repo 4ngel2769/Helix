f5535a217ff624f9087a41e93efa861645cad602
Sypher0 <78003275+4ngel2769@users.noreply.github.com>
2023-12-17 17:53:23 +0200

    updateindexjs

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits } from 'discord.js';
+
+const client = new SapphireClient({
+    intents: [GatewayIntentBits.MessageContent, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+    loadMessageCommandListeners: true
+});
+
+client.login('your-token-goes-here');
