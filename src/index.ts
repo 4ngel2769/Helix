7fce15ada13c8ef2b34d3ac5b6f0032fe37b04cf
Sypher0 <78003275+4ngel2769@users.noreply.github.com>
2024-05-10 18:58:45 +0300

    changes

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@ import './lib/setup';
 
 import { LogLevel, SapphireClient } from '@sapphire/framework';
 import { GatewayIntentBits, Partials } from 'discord.js';
+import { env } from 'process';
+// import Keyv from 'keyv';
+
+// import SoftUI  from 'dbd-soft-ui';
+// import config from '../config.json';
+
+// let DBD = require('discord-dashboard');
 
 const client = new SapphireClient({
-	defaultPrefix: 'x',
+	defaultPrefix: env.PREFIX,
 	regexPrefix: /^(hey +)?bot[,! ]/i,
 	caseInsensitiveCommands: false,
 	logger: {
@@ -26,6 +33,84 @@ const client = new SapphireClient({
 	partials: [Partials.Channel],
 	loadMessageCommandListeners: true
 });
+
+// const Handler = new DBD.Handler(
+// );
+
+// (async ()=>{
+//     await DBD.useLicense(config.dashboard.license);
+//     DBD.Dashboard = DBD.UpdatedClass();
+
+//     const Dashboard = new DBD.Dashboard({
+//         port: config.dashboard.port,
+//         client: config.bot.client,
+//         redirectUri: `${config.dashboard.domain}${config.dashboard.redirectUri}`,
+//         domain: config.dashboard.domain,
+//         ownerIDs: config.dashboard.ownerIDs,
+//         useThemeMaintenance: true,
+//         useTheme404: true,
+//         bot: client,
+// 		acceptPrivacyPolicy: true,
+//         theme: SoftUI({
+//             storage: Handler,
+//             customThemeOptions: {
+//                 index: async ({ }) => {
+//                     return {
+//                         values: [],
+//                         graph: {},
+//                         cards: [],
+//                     }
+//                 },
+//             },
+//             websiteName: "Helix",
+//             colorScheme: "blue",
+//             supporteMail: config.dashboard.supportMail,
+//             icons: {
+// 				backgroundImage: '',
+//                 favicon: '../src/db/assets/branding/helix.png',
+//                 noGuildIcon: '../src/db/assets/branding/logo.png',
+//                 sidebar: {
+//                     darkUrl: config.dashboard.ui.darkLogo,
+//                     lightUrl: config.dashboard.ui.lightLogo,
+//                     hideName: false,
+//                     borderRadius: false,
+//                     alignCenter: true
+//                 },
+//             },
+//             index: {
+//                 graph: {
+//                     enabled: true,
+//                     lineGraph: true,
+//                     title: 'Memory Usage',
+//                     tag: 'Memory (MB)',
+//                     max: 300
+//                 },
+//             },
+//             sweetalert: {
+//                 errors: {},
+//                 success: {
+//                     login: "Successfully logged in.",
+//                 }
+//             },
+//             preloader: {
+//                 image: config.dashboard.ui.preload,
+//                 spinner: false,
+//                 text: "welcome...",
+//             },
+//             admin: {
+//                 pterodactyl: {
+//                     enabled: false,
+//                     apiKey: "apiKey",
+//                     panelLink: "https://panel.website.com",
+//                     serverUUIDs: []
+//                 }
+//             },
+//             commands: [],
+//         }),
+//         settings: []
+//     });
+//     Dashboard.init();
+// })();
 
 const main = async () => {
 	try {
